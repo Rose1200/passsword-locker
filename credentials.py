5ae0babe8d7140870bf25ec68650112bdae0d0c5
Rose <rosekamau.student@moringaschool.com>
2022-05-05 15:24:46 +0300

    add credential lists

--- credentials.py
+++ credentials.py
@@ -3,3 +3,11 @@ class Credentials:
     Class to generate instances of Credentials
     """    
     credentials_list = []
+
+    def __init__(self, account_name, account_user_name, account_password):
+        '''
+        defines properties for our credentials objects.
+        '''
+        self.account_name = account_name
+        self.account_user_name = account_user_name
+        self.account_password = account_password
