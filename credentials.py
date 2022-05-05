a23107d36479a4e9fe1e1b8e04dcaa08101f87a7
Rose <rosekamau.student@moringaschool.com>
2022-05-05 16:05:29 +0300

    add classmethod

--- credentials.py
+++ credentials.py
@@ -11,3 +11,33 @@ class Credentials:
         self.account_name = account_name
         self.account_user_name = account_user_name
         self.account_password = account_password
+def add_credentials(self):
+        '''
+        adds new credentials to the list
+        '''
+        Credentials.credentials_list.append(self)
+          
+          
+@classmethod
+def display_all_accounts(cls):
+        '''
+        returns the credentials list
+        '''
+        return cls.credentials_list
+ 
+@classmethod
+def find_by_account(cls, name):
+        '''
+        find_by_account method takes a searched account name and returns account credentials of that account.
+
+        Parameter/Argument:
+            name: Account name to search for
+        Returns :
+            Credentials of the user account that matches the searched account.
+        '''
+        for account in cls.credentials_list:
+            if account.account_name == name:
+                return account
+
+
+
