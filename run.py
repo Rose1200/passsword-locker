6d97a53815e137342feacca5a60139061cea64b9
Rose <rosekamau.student@moringaschool.com>
2022-05-05 15:15:54 +0300

    add function

--- run.py
+++ run.py
@@ -3,4 +3,12 @@ from user import User
 from credentials import Credentials
 import random  
 
+def cr_user(user_name, password):
+    """
+    Function to create a new user
+    """
+    new_user = User(user_name, password)
+    return new_user
+
+
 
